3e35c59e76fb8c9762b45f660a5d1c67a68d9800
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-09 20:26:14 -0300

    feat: add useCreateRoom custom hook

---
+++ src/hooks/useCreateRoom.tsx
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { CreateRoomRequest } from "@/@types/create-room-request";
+import type { CreateRoomResponse } from "@/@types/create-room-response";
+import { api } from "@/lib/axios";
+
+export const useCreateRoom = () => {
+  const queryClient = useQueryClient();
+
+  const createRoom = async (
+    data: CreateRoomRequest
+  ): Promise<CreateRoomResponse> => {
+    const res = await api.post("/rooms", data);
+
+    return res.data;
+  };
+
+  return useMutation({
+    mutationFn: createRoom,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["get-rooms"] });
+    },
+  });
+};
